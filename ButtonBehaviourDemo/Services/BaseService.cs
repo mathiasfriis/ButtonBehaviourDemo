44da8ce591ae7916e040505a1622df380238fcec
MathiasFriis <mathiasfriis1995@hotmail.com>
2025-05-27 15:16:44 +0200

    Comment BaseService.

--- ButtonBehaviourDemo/Services/BaseService.cs
+++ ButtonBehaviourDemo/Services/BaseService.cs
@@ -5,30 +5,33 @@ using System;
 
 public abstract class BaseService : IService
 {
-    protected readonly EventBus _bus;
-    private readonly ActionQueue _queue = new();
-    private int _intervalMs;
-    private CancellationTokenSource _cts;
+    protected readonly EventBus _bus; // EventBus shared between all services.
+    private readonly ActionQueue _queue = new(); // Separate ActionQueue for each service to handle its own events.
+    private int _intervalMs; // Interval in milliseconds for periodic service execution.
+    private CancellationTokenSource _cts; // Cancellation token source to manage the service lifecycle.
 
     protected BaseService(EventBus bus)
     {
         _bus = bus;
     }
 
-    public void HandleEvents() => _queue.Process();
+    public void HandleEvents() => _queue.Process(); // Process all events in the queue. This method is public for testing purposes.
 
     public virtual void Service() { } // Hook for any logic that needs to be executed periodically. Public for testing purposes.
 
+    // Subscribe to events of type T. Messages should be processed by the parsed handler.
     protected void Subscribe<T>(Action<T> handler) where T : IEvent
     {
         _bus.Subscribe(handler, _queue);
     }
 
+    // Publish an event of type T. This will notify all subscribers of the event.
     protected void Publish<T>(T evt) where T : IEvent
     {
         _bus.Publish(evt);
     }
 
+    // Start service in a separate thread, processing events at the specified interval.
     public void Start(int intervalMs)
     {
         _intervalMs = intervalMs;
@@ -38,13 +41,14 @@ public abstract class BaseService : IService
         {
             while (!_cts.Token.IsCancellationRequested)
             {
-                HandleEvents();
-                Service(); // Call the periodic service method.
+                HandleEvents(); // Handle any events in the queue.
+                Service(); // Call the periodic service method. TODO: Should we have hooks pre and post event handling?
                 await Task.Delay(_intervalMs, _cts.Token);
             }
         });
     }
 
+    // Stop service.
     public void Stop()
     {
         _cts?.Cancel();
